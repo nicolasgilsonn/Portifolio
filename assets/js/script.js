669ed036f65a858e8f59ff8ee0d06731746fd06b
nicolasgilsonn <nicolasgilson55@gmail.com>
2024-03-27 09:38:50 -0300

    27/03

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+document.querySelector("#memoria").addEventListener("change", calcular)
+document.querySelector("#tipo").addEventListener("change", calcular)
+document.querySelector("#prototipo_sim").addEventListener("change", calcular)
+document.querySelector("#prazo").addEventListener("input", function(){
+    const pazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerText = "Prazo: " + prazo
+    calcular()
+})
+
+function calcular(){
+    const qtdM = document.querySelector("#memoria").value
+    let valor = qtdM * 200
+    
+
+    const tipo = document.querySelector("#tipo").value
+    if(tipo == 2) valor += 700
+
+    const prototipo = document.querySelector("#prototipo_sim").checked
+    if(prototipo) valor += 1500
+
+    document.querySelector("#valor").innerText ='R$' + valor.toFixed(2)
+
+
+
+}
